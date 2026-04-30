2cb53a476ba42bdf620764079ab23bf412948277
evbomwan <evbomwanc@gmail.com>
2026-04-30 20:54:00 +0100

    added the playGame function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 function getComputerChoice() {
   if (Math.random() < 0.33) {
     return "rock";
@@ -12,13 +11,27 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Please enter rock, paper, or scissors.").toLocaleLowerCase();
 }
+
+
+function playGame(){
+let humanScore = 0;
+let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     return "It's a tie!";
   } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++
     return "You win! " + humanChoice + " beats " + computerChoice + ".";
 } else {
+  computerScore++
   return "You lose! " + computerChoice + " beats " + humanChoice + ".";
 }
 }
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+for (i = 0; i < 5; i++) {
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+  console.log("Your score: " + humanScore + " Computer score: " + computerScore);
+}
+}
+playGame();
